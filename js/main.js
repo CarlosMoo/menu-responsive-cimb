f9891b96f7761cc9eb3a7a45c26393d46aa65723
Carlos Moo <LE17081431@merida.tecnm.mx>
2021-11-15 15:15:06 -0600

    add directions for navigate

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
  $(document).ready(function(){
 
- 	// MOSTRANDO Y OCULTANDO MENU
  	$('#removeicon').click(function(){
  		if($('#removeicon').attr('class') == 'fas fa-bars' ){
 
- 			$('#removeicon').removeClass('fas fa-bars').addClass('fas fa-times-circle'); // Agregamos el icono X
- 			$('.navegacion .menu').css({'left':'0px', 'transition': 'all 0.5s'}); // Mostramos el menu
+ 			$('#removeicon').removeClass('fas fa-bars').addClass('fas fa-times-circle');
+ 			$('.navegacion .menu').css({'left':'0px', 'transition': 'all 0.5s'});
 
  		} else{
 
-			$('#removeicon').removeClass('fas fa-times-circle').addClass('fas fa-bars'); // Agregamos el icono del Menu
- 			$('.navegacion .menu').css({'left':'-414px', 'transition': 'all 0.5s'}); // Ocultamos el Menu
-
+			$('#removeicon').removeClass('fas fa-times-circle').addClass('fas fa-bars');
+ 			$('.navegacion .menu').css({'left':'-414px', 'transition': 'all 0.5s'});
 		}
  	});
 
